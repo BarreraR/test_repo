2b851b84d1816a84241bcdb0734682a2a7cd6132
Raul Barrera, Jr <BarreraR@users.noreply.github.com>
2020-09-10 13:59:32 -0500

    Revert "Theres no syrup"

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
+'use strict';
 
-let myTeam = "Raul, Brendon"
-console.log(myTeam)
-
-console.log("Just lemme pass man")
+let myTeam = 'Raul, Brendon';
+console.log(myTeam);
 
 for (let index = 0; index < 10; index+=2) {
   console.log(index);
     
 }
-
